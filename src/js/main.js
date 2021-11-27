63e680ddb025b8f948491ad00efdbfa898430816
Jan Kupczyk <73750630+jankupczyk@users.noreply.github.com>
2021-11-28 00:57:22 +0100

    Add files via upload

--- src/js/main.js
+++ src/js/main.js
@@ -135,7 +135,7 @@ function updateVisitCount() {
 		})
 }
 
-$(function () {
+$(function() {
 	$('.scroll-down').click(function () {
 		$('html, body').animate({ scrollTop: $('section#about').offset().top }, 'slow');
 		return false;
