dcaa670367294a9d1f55fca112a531712de666fa
Jan Kupczyk <73750630+jankupczyk@users.noreply.github.com>
2022-05-29 21:40:12 +0200

    Add files via upload

--- src/js/btn.js
+++ src/js/btn.js
@@ -1,39 +1,3 @@
-function BSsmbtn() {
-    Swal.fire({
-        title: '<strong>Banco Santander</strong>',
-        backdrop: `rgba(0, 0, 0, 1);`,
-        imageUrl: 'public/assets/photos/webp/santander.webp',
-        imageWidth: 90,
-        imageHeight: 90,
-        imageAlt: 'Banco Santander',
-        html:
-            '<span class="w-ctegory" style="color: crimson;">IT Services, Monitoring ◦Internship</span>' +
-            '<ul>BRIDGE:</ul>' +
-            '<li>REMEDY</li>' +
-            '<li>ZABBIX</li>' +
-            '<li>GrayLog</li>' +
-            '<li>"Verify" policy</li>' +
-            '<li>DMZ and contact with external companies</li>' +
-            '<li>exKB branch links</li>' +
-            '<li>Backup data verification</li>' +
-            '<li>CORTEX</li>' +
-            '<li>Orion</li>' +
-            '<li>BMC</li>' +
-            '<li>List and docket of disks and tapes</li>' +
-            '<li>CMDB_INFRA enumeration</li>' +
-            '<li>Telephone service support (ATM)</li>' +
-            '<br>' +
-            '<ul>IT service departament:</ul>' +
-            '<li>JSON</li>' +
-            '<li>cURL</li>' +
-            '<li>JQ</li>' +
-            '<br>' +
-            '<span class="w-date" style="color: crimson; font-size: .8rem;">March 2020 - April 2020 ◂ 2 months</span>',
-        focusConfirm: false,
-    })
-
-}
-
 function HTsmbtn() {
     Swal.fire({
         title: '<strong>Hardsoft-Telekom</strong>',
